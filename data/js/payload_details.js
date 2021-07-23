7da9dab08e1fc391910aef508852cae380404797
wynkenhimself <wynkenhimself@users.noreply.github.com>
2021-07-22 22:15:42 -0400

    Added 2 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 72764,
-	"created_at": "Thu Jul 22 02:15:42 +0000 2021",
+	"tweets": 72766,
+	"created_at": "Fri Jul 23 02:15:41 +0000 2021",
 	"lang": "en"
 }
--- data/js/tweets/2021_07.js
+++ data/js/tweets/2021_07.js
@@ -1,6 +1,219 @@
 Grailbird.data.tweets_2021_07 = 
 [
 	{
+		"created_at": "Fri Jul 23 00:13:03 +0000 2021",
+		"id": 1418359588622160000,
+		"id_str": "1418363495242899456",
+		"full_text": "I’d love to read reported pieces on the unionization drives at Oxford and Duke University Presses.",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			98
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": []
+		},
+		"source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 1645490090,
+			"id_str": "1645490090",
+			"name": "Derek Krissoff",
+			"screen_name": "DerekKrissoff",
+			"location": "Pittsburgh and Morgantown",
+			"description": "Director at West Virginia University Press (@WVUPRESS). New Pittsburgher. Digital skeptic. Staff. All views are etc.",
+			"url": "https://t.co/AIXvb8zVcA",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/AIXvb8zVcA",
+							"expanded_url": "https://krissedoff.wordpress.com",
+							"display_url": "krissedoff.wordpress.com",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 4398,
+			"friends_count": 3955,
+			"listed_count": 77,
+			"created_at": "Sun Aug 04 15:32:03 +0000 2013",
+			"favourites_count": 28310,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 9925,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "C0DEED",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/609000763499642882/305Ci2GY_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/609000763499642882/305Ci2GY_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/1645490090/1486596110",
+			"profile_link_color": "1DA1F2",
+			"profile_sidebar_border_color": "C0DEED",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": true,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 8,
+		"favorite_count": 71,
+		"favorited": false,
+		"retweeted": true,
+		"lang": "en",
+		"text": "I’d love to read reported pieces on the unionization drives at Oxford and Duke University Presses."
+	},
+	{
+		"created_at": "Thu Jul 22 23:24:16 +0000 2021",
+		"id": 1418351218536112000,
+		"id_str": "1418351218536112128",
+		"full_text": "@revmarissa It really is and I wouldn’t trade it for anything",
+		"truncated": false,
+		"display_text_range": [
+			12,
+			61
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "revmarissa",
+					"name": "The Rev. Marissa Rohrbach 🌈 (she/her)",
+					"id": 4354346777,
+					"id_str": "4354346777",
+					"indices": [
+						0,
+						11
+					]
+				}
+			],
+			"urls": []
+		},
+		"source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+		"in_reply_to_status_id": 1418349713674449000,
+		"in_reply_to_status_id_str": "1418349713674448899",
+		"in_reply_to_user_id": 4354346777,
+		"in_reply_to_user_id_str": "4354346777",
+		"in_reply_to_screen_name": "revmarissa",
+		"user": {
+			"id": 35384223,
+			"id_str": "35384223",
+			"name": "Sarah Werner",
+			"screen_name": "wynkenhimself",
+			"location": "Washington, DC",
+			"description": "feminist bibliography, libraries, old books | Studying Early Printed Books 1450-1800 | https://t.co/aDAGu7rvJP | #TeamBacchae | she/her ✡️ 🌈 ♑️",
+			"url": "https://t.co/5cZS3cbqcx",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/5cZS3cbqcx",
+							"expanded_url": "http://sarahwerner.net",
+							"display_url": "sarahwerner.net",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": [
+						{
+							"url": "https://t.co/aDAGu7rvJP",
+							"expanded_url": "https://linktr.ee/wynkenhimself",
+							"display_url": "linktr.ee/wynkenhimself",
+							"indices": [
+								87,
+								110
+							]
+						}
+					]
+				}
+			},
+			"protected": false,
+			"followers_count": 9855,
+			"friends_count": 457,
+			"listed_count": 383,
+			"created_at": "Sun Apr 26 02:57:49 +0000 2009",
+			"favourites_count": 37351,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 72564,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "3B2B18",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme9/bg.gif",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme9/bg.gif",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/839810650230632449/aWo5oO-A_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/839810650230632449/aWo5oO-A_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/35384223/1551722011",
+			"profile_link_color": "2F7BED",
+			"profile_sidebar_border_color": "FFFFFF",
+			"profile_sidebar_fill_color": "252429",
+			"profile_text_color": "666666",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 0,
+		"favorite_count": 1,
+		"favorited": false,
+		"retweeted": false,
+		"lang": "en",
+		"text": "@revmarissa It really is and I wouldn’t trade it for anything"
+	},
+	{
 		"created_at": "Wed Jul 21 14:36:17 +0000 2021",
 		"id": 1417855958965948400,
 		"id_str": "1417855958965948419",
