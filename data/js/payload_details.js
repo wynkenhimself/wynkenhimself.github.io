a58a4686372d17f651d17db83bbc505effff1fe0
wynkenhimself <wynkenhimself@users.noreply.github.com>
2021-10-28 22:15:42 -0400

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 73626,
-	"created_at": "Thu Oct 28 02:15:40 +0000 2021",
+	"tweets": 73627,
+	"created_at": "Fri Oct 29 02:15:41 +0000 2021",
 	"lang": "en"
 }
--- data/js/tweets/2021_10.js
+++ data/js/tweets/2021_10.js
@@ -1,6 +1,181 @@
 Grailbird.data.tweets_2021_10 = 
 [
 	{
+		"created_at": "Thu Oct 28 02:17:05 +0000 2021",
+		"id": 1453538326485500000,
+		"id_str": "1453546330660524033",
+		"full_text": "live your life in such a way that you never trend with nazis https://t.co/eyp0P4TKQn",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			60
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [],
+			"media": [
+				{
+					"id": 1453537360604369000,
+					"id_str": "1453537360604368896",
+					"indices": [
+						61,
+						84
+					],
+					"media_url": "http://pbs.twimg.com/media/FCwAhz6VEAAvOvS.png",
+					"media_url_https": "https://pbs.twimg.com/media/FCwAhz6VEAAvOvS.png",
+					"url": "https://t.co/eyp0P4TKQn",
+					"display_url": "pic.twitter.com/eyp0P4TKQn",
+					"expanded_url": "https://twitter.com/goldengateblond/status/1453538326485499906/photo/1",
+					"type": "photo",
+					"sizes": {
+						"small": {
+							"w": 374,
+							"h": 164,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 374,
+							"h": 164,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 374,
+							"h": 164,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1453537360604369000,
+					"id_str": "1453537360604368896",
+					"indices": [
+						61,
+						84
+					],
+					"media_url": "http://pbs.twimg.com/media/FCwAhz6VEAAvOvS.png",
+					"media_url_https": "https://pbs.twimg.com/media/FCwAhz6VEAAvOvS.png",
+					"url": "https://t.co/eyp0P4TKQn",
+					"display_url": "pic.twitter.com/eyp0P4TKQn",
+					"expanded_url": "https://twitter.com/goldengateblond/status/1453538326485499906/photo/1",
+					"type": "photo",
+					"sizes": {
+						"small": {
+							"w": 374,
+							"h": 164,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 374,
+							"h": 164,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 374,
+							"h": 164,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Web App</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 15576928,
+			"id_str": "15576928",
+			"name": "shauna",
+			"screen_name": "goldengateblond",
+			"location": "san francisco",
+			"description": "“She’s the big homie. Like, when she walks in a room you stand up.” — @rembert",
+			"url": "https://t.co/60l9QVVSTt",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/60l9QVVSTt",
+							"expanded_url": "https://votesaveamerica.com/",
+							"display_url": "votesaveamerica.com",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 362033,
+			"friends_count": 1731,
+			"listed_count": 3236,
+			"created_at": "Thu Jul 24 03:44:09 +0000 2008",
+			"favourites_count": 296505,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": true,
+			"statuses_count": 78078,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "352726",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme5/bg.gif",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme5/bg.gif",
+			"profile_background_tile": true,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/548586934319071232/JkfxEiRT_normal.jpeg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/548586934319071232/JkfxEiRT_normal.jpeg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/15576928/1487898459",
+			"profile_link_color": "960014",
+			"profile_sidebar_border_color": "1F2616",
+			"profile_sidebar_fill_color": "D5DE6D",
+			"profile_text_color": "000000",
+			"profile_use_background_image": true,
+			"has_extended_profile": true,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "regular",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 2148,
+		"favorite_count": 16497,
+		"favorited": true,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "live your life in such a way that you never trend with nazis https://t.co/eyp0P4TKQn"
+	},
+	{
 		"created_at": "Wed Oct 27 23:55:53 +0000 2021",
 		"id": 1453510798219874300,
 		"id_str": "1453510798219874310",
