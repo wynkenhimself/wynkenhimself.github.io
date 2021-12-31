7ff63e4ad50a9357fb332a20e9a99ac834ea09f0
wynkenhimself <wynkenhimself@users.noreply.github.com>
2021-12-30 21:15:41 -0500

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 73977,
-	"created_at": "Thu Dec 30 02:15:40 +0000 2021",
+	"tweets": 73978,
+	"created_at": "Fri Dec 31 02:15:40 +0000 2021",
 	"lang": "en"
 }
--- data/js/tweets/2021_12.js
+++ data/js/tweets/2021_12.js
@@ -1,6 +1,107 @@
 Grailbird.data.tweets_2021_12 = 
 [
 	{
+		"created_at": "Thu Dec 30 03:24:06 +0000 2021",
+		"id": 1476393385875230700,
+		"id_str": "1476393631351246848",
+		"full_text": "Testing hasn’t “slowed”—it’s become nearly impossible in hard-hit areas and/or financially burdensome and/or unreliable and/or too time-consuming to seek out. The failings are many and this number is a national tragedy. https://t.co/1YXweJwRDJ",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			219
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/1YXweJwRDJ",
+					"expanded_url": "https://twitter.com/nytimes/status/1476385926129590273",
+					"display_url": "twitter.com/nytimes/status…",
+					"indices": [
+						220,
+						243
+					]
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 15145016,
+			"id_str": "15145016",
+			"name": "Rahawa Haile",
+			"screen_name": "RahawaHaile",
+			"location": "Ohlone Land (Oakland, CA)",
+			"description": "Eritrean American writer from Miami. In Open Country forthcoming from @harperbooks (2023). Repped by @jlkardon. she/her",
+			"url": null,
+			"entities": {
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 15064,
+			"friends_count": 1625,
+			"listed_count": 243,
+			"created_at": "Tue Jun 17 12:33:48 +0000 2008",
+			"favourites_count": 30702,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 22939,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "FFFFFF",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": true,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1196879406746071040/4vj1brcL_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1196879406746071040/4vj1brcL_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/15145016/1638283138",
+			"profile_link_color": "0000FF",
+			"profile_sidebar_border_color": "87BC44",
+			"profile_sidebar_fill_color": "E0FF92",
+			"profile_text_color": "000000",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": true,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": true,
+		"quoted_status_id": 1476385926129590300,
+		"quoted_status_id_str": "1476385926129590273",
+		"quoted_status_permalink": {
+			"url": "https://t.co/1YXweJwRDJ",
+			"expanded": "https://twitter.com/nytimes/status/1476385926129590273",
+			"display": "twitter.com/nytimes/status…"
+		},
+		"retweet_count": 7,
+		"favorite_count": 40,
+		"favorited": true,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "Testing hasn’t “slowed”—it’s become nearly impossible in hard-hit areas and/or financially burdensome and/or unreliable and/or too time-consuming to seek out. The failings are many and this number is a national tragedy. https://t.co/1YXweJwRDJ"
+	},
+	{
 		"created_at": "Thu Dec 30 02:13:53 +0000 2021",
 		"id": 1476375961113346000,
 		"id_str": "1476375961113346048",
