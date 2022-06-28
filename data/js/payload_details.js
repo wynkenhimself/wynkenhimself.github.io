0e40c7379009b5669ad8b266ea1c369738bf0c30
wynkenhimself <wynkenhimself@users.noreply.github.com>
2022-06-27 22:15:42 -0400

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 75398,
-	"created_at": "Mon Jun 27 02:15:41 +0000 2022",
+	"tweets": 75399,
+	"created_at": "Tue Jun 28 02:15:41 +0000 2022",
 	"lang": "en"
 }
--- data/js/tweets/2022_06.js
+++ data/js/tweets/2022_06.js
@@ -1,6 +1,260 @@
 Grailbird.data.tweets_2022_06 = 
 [
 	{
+		"created_at": "Mon Jun 27 18:28:14 +0000 2022",
+		"id": 1541406240512811000,
+		"id_str": "1541488592165474308",
+		"full_text": "Texts and images of the Shakespeare Quartos Archive (formerly https://t.co/ASUvo1aKxK) are now available in #PRISMS.  The SQA is a digital collection of 32 copies of the 5 quarto edns of #WilliamShakespeare's #Hamlet: \n\nhttps://t.co/1aNz3FLDIl\n \n#TEI #DSE #DH #DigitalHumanities https://t.co/CseEtEiVEU",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			278
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "PRISMS",
+					"indices": [
+						108,
+						115
+					]
+				},
+				{
+					"text": "WilliamShakespeare",
+					"indices": [
+						187,
+						206
+					]
+				},
+				{
+					"text": "Hamlet",
+					"indices": [
+						209,
+						216
+					]
+				},
+				{
+					"text": "TEI",
+					"indices": [
+						246,
+						250
+					]
+				},
+				{
+					"text": "DSE",
+					"indices": [
+						251,
+						255
+					]
+				},
+				{
+					"text": "DH",
+					"indices": [
+						256,
+						259
+					]
+				},
+				{
+					"text": "DigitalHumanities",
+					"indices": [
+						260,
+						278
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/ASUvo1aKxK",
+					"expanded_url": "http://quartos.org",
+					"display_url": "quartos.org",
+					"indices": [
+						62,
+						85
+					]
+				},
+				{
+					"url": "https://t.co/1aNz3FLDIl",
+					"expanded_url": "https://www.prisms.digital/collection/sqa",
+					"display_url": "prisms.digital/collection/sqa",
+					"indices": [
+						220,
+						243
+					]
+				}
+			],
+			"media": [
+				{
+					"id": 1541392476908540000,
+					"id_str": "1541392476908539904",
+					"indices": [
+						279,
+						302
+					],
+					"media_url": "http://pbs.twimg.com/media/FWQgSoWX0AAqQ6n.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/FWQgSoWX0AAqQ6n.jpg",
+					"url": "https://t.co/CseEtEiVEU",
+					"display_url": "pic.twitter.com/CseEtEiVEU",
+					"expanded_url": "https://twitter.com/Huber_Digital/status/1541406240512811008/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 680,
+							"h": 344,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 1200,
+							"h": 607,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 607,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1541392476908540000,
+					"id_str": "1541392476908539904",
+					"indices": [
+						279,
+						302
+					],
+					"media_url": "http://pbs.twimg.com/media/FWQgSoWX0AAqQ6n.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/FWQgSoWX0AAqQ6n.jpg",
+					"url": "https://t.co/CseEtEiVEU",
+					"display_url": "pic.twitter.com/CseEtEiVEU",
+					"expanded_url": "https://twitter.com/Huber_Digital/status/1541406240512811008/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 680,
+							"h": 344,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 1200,
+							"h": 607,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 607,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"https://about.twitter.com/products/tweetdeck\" rel=\"nofollow\">TweetDeck</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 1190692605597769700,
+			"id_str": "1190692605597769730",
+			"name": "Huber Digital",
+			"screen_name": "Huber_Digital",
+			"location": "Oxford, UK",
+			"description": "We provide web development and training with a difference for academic #DigitalHumanities projects.  We built the Open Scholarship platform https://t.co/WvftWWaPDR",
+			"url": "https://t.co/X5hZ1LPFyS",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/X5hZ1LPFyS",
+							"expanded_url": "https://hubers.org.uk/",
+							"display_url": "hubers.org.uk",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": [
+						{
+							"url": "https://t.co/WvftWWaPDR",
+							"expanded_url": "http://www.prisms.digital",
+							"display_url": "prisms.digital",
+							"indices": [
+								140,
+								163
+							]
+						}
+					]
+				}
+			},
+			"protected": false,
+			"followers_count": 498,
+			"friends_count": 456,
+			"listed_count": 10,
+			"created_at": "Sat Nov 02 18:11:32 +0000 2019",
+			"favourites_count": 5553,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 174,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "F5F8FA",
+			"profile_background_image_url": null,
+			"profile_background_image_url_https": null,
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1190693491162189824/YT5wW124_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1190693491162189824/YT5wW124_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/1190692605597769730/1593008674",
+			"profile_link_color": "1DA1F2",
+			"profile_sidebar_border_color": "C0DEED",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": true,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 16,
+		"favorite_count": 28,
+		"favorited": true,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "Texts and images of the Shakespeare Quartos Archive (formerly https://t.co/ASUvo1aKxK) are now available in #PRISMS.  The SQA is a digital collection of 32 copies of the 5 quarto edns of #WilliamShakespeare's #Hamlet: \n\nhttps://t.co/1aNz3FLDIl\n \n#TEI #DSE #DH #DigitalHumanities https://t.co/CseEtEiVEU"
+	},
+	{
 		"created_at": "Mon Jun 27 00:51:42 +0000 2022",
 		"id": 1541222707253182500,
 		"id_str": "1541222707253182466",
