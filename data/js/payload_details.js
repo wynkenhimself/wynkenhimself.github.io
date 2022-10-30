ca64b265cbc42dcb48bc931373b80a94d0cd1e65
wynkenhimself <wynkenhimself@users.noreply.github.com>
2022-10-29 22:15:42 -0400

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 76301,
-	"created_at": "Sat Oct 29 02:15:42 +0000 2022",
+	"tweets": 76302,
+	"created_at": "Sun Oct 30 02:15:41 +0000 2022",
 	"lang": "en"
 }
--- data/js/tweets/2022_10.js
+++ data/js/tweets/2022_10.js
@@ -1,6 +1,99 @@
 Grailbird.data.tweets_2022_10 = 
 [
 	{
+		"created_at": "Sat Oct 29 14:16:35 +0000 2022",
+		"id": 1586357345495699500,
+		"id_str": "1586361359234904064",
+		"full_text": "A thing I wrote about the evolution of the MLA citation format https://t.co/PqbAWRLFfL",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			86
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/PqbAWRLFfL",
+					"expanded_url": "https://doi.org/10.1515/9783110748819-002",
+					"display_url": "doi.org/10.1515/978311…",
+					"indices": [
+						63,
+						86
+					]
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 65899075,
+			"id_str": "65899075",
+			"name": "Lisa Gitelman",
+			"screen_name": "namleti",
+			"location": "",
+			"description": "media history whatnot, trying to think the space between English and media studies, for fun for students & for the humanities",
+			"url": null,
+			"entities": {
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 1850,
+			"friends_count": 1066,
+			"listed_count": 22,
+			"created_at": "Sat Aug 15 13:44:20 +0000 2009",
+			"favourites_count": 12866,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 1131,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "C0DEED",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/363949608/IMG_0165_normal.JPG",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/363949608/IMG_0165_normal.JPG",
+			"profile_link_color": "1DA1F2",
+			"profile_sidebar_border_color": "C0DEED",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": true,
+			"default_profile_image": false,
+			"following": true,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 10,
+		"favorite_count": 20,
+		"favorited": true,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "A thing I wrote about the evolution of the MLA citation format https://t.co/PqbAWRLFfL"
+	},
+	{
 		"created_at": "Fri Oct 28 13:03:38 +0000 2022",
 		"id": 1585720713628987400,
 		"id_str": "1585980610300055558",
