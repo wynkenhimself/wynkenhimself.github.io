884547eecd9bd4cbe76ae38411ba650c024d194f
wynkenhimself <wynkenhimself@users.noreply.github.com>
2023-03-25 22:15:41 -0400

    Added 1 tweets

--- data/js/tweets/2023_03.js
+++ data/js/tweets/2023_03.js
@@ -1,6 +1,141 @@
 Grailbird.data.tweets_2023_03 = 
 [
 	{
+		"created_at": "Sat Mar 25 14:45:07 +0000 2023",
+		"id": 1639639553785970700,
+		"id_str": "1639639553785970689",
+		"full_text": "@natmcgartland @Bibliographes @book_historia Hiiiiii yes!! That was one of my favorite moments too and I think about it a lot when thinking about parameters of #FemBib! That was such a fun conversation",
+		"truncated": false,
+		"display_text_range": [
+			45,
+			201
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "FemBib",
+					"indices": [
+						160,
+						167
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "natmcgartland",
+					"name": "Nat McGartland",
+					"id": 921056157250224100,
+					"id_str": "921056157250224129",
+					"indices": [
+						0,
+						14
+					]
+				},
+				{
+					"screen_name": "Bibliographes",
+					"name": "Erin McGuirl",
+					"id": 259097345,
+					"id_str": "259097345",
+					"indices": [
+						15,
+						29
+					]
+				},
+				{
+					"screen_name": "book_historia",
+					"name": "Allie Alvis 🚄",
+					"id": 138620865,
+					"id_str": "138620865",
+					"indices": [
+						30,
+						44
+					]
+				}
+			],
+			"urls": []
+		},
+		"source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+		"in_reply_to_status_id": 1639639112171892700,
+		"in_reply_to_status_id_str": "1639639112171892738",
+		"in_reply_to_user_id": 921056157250224100,
+		"in_reply_to_user_id_str": "921056157250224129",
+		"in_reply_to_screen_name": "natmcgartland",
+		"user": {
+			"id": 35384223,
+			"id_str": "35384223",
+			"name": "Sarah Werner",
+			"screen_name": "wynkenhimself",
+			"location": "Washington, DC",
+			"description": "not here anymore | @wynkenhimself@glammr.us ✡️ 🌈 ♑️",
+			"url": "https://t.co/cwZjQb5axE",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/cwZjQb5axE",
+							"expanded_url": "http://sarahwerner.net",
+							"display_url": "sarahwerner.net",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": true,
+			"followers_count": 9763,
+			"friends_count": 444,
+			"listed_count": 366,
+			"created_at": "Sun Apr 26 02:57:49 +0000 2009",
+			"favourites_count": 41974,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 72978,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "3B2B18",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme9/bg.gif",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme9/bg.gif",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/839810650230632449/aWo5oO-A_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/839810650230632449/aWo5oO-A_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/35384223/1551722011",
+			"profile_link_color": "2F7BED",
+			"profile_sidebar_border_color": "FFFFFF",
+			"profile_sidebar_fill_color": "252429",
+			"profile_text_color": "666666",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 0,
+		"favorite_count": 3,
+		"favorited": false,
+		"retweeted": false,
+		"lang": "en",
+		"text": "@natmcgartland @Bibliographes @book_historia Hiiiiii yes!! That was one of my favorite moments too and I think about it a lot when thinking about parameters of #FemBib! That was such a fun conversation"
+	},
+	{
 		"created_at": "Tue Mar 21 22:18:02 +0000 2023",
 		"id": 1638212466264948700,
 		"id_str": "1638303981725204480",
